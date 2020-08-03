4a3b81b7d82352dbac91145e0f314237d621cf8e
Malte <malte.neuendorf@yahoo.de>
2020-08-03 16:27:52 +0200

    Updates

---
+++ precache-manifest.4f8a95310b1026f03eca47281a35a7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb93bd22d17291efce076cecdc428d7",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "a85bf3cd6539a25242f2",
+    "url": "/MonsterRolodex/static/css/main.5832b74c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonsterRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85bf3cd6539a25242f2",
+    "url": "/MonsterRolodex/static/js/main.1b4224f6.chunk.js"
+  },
+  {
+    "revision": "35754fcbc87d98e43b53",
+    "url": "/MonsterRolodex/static/js/runtime-main.751de5f8.js"
+  }
+]);
